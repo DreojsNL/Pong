15a348f15a6a1b67588305933c16c4f1931d8026
Dreojs-NL <58047063+Dreojs-NL@users.noreply.github.com>
2022-09-16 11:59:54 +0200

    added incline
    And secret

--- Pong1/Assets/Scripts/MoveBall.cs
+++ Pong1/Assets/Scripts/MoveBall.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class MoveBall : MonoBehaviour
 {
+    public float ForceBall = 600f;
+
 
     public Rigidbody2D rb;
     void Start()
     {
+
         rb = GetComponent<Rigidbody2D>();
         rb.AddForce(Vector2.right * 600);
+        rb.AddForce(Vector2.up * 200);
     }
 }
