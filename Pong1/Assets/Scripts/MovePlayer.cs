b849fc94467ce5c0965aac1df90d5f63401e8ca2
Dreojs-NL <58047063+Dreojs-NL@users.noreply.github.com>
2022-09-19 14:26:40 +0200

    Fixt bugs
    Paddles stoppen nu bij de rant en de phisics zijn gefixt

--- Pong1/Assets/Scripts/MovePlayer.cs
+++ Pong1/Assets/Scripts/MovePlayer.cs
@@ -11,17 +11,36 @@ public class MovePlayer : MonoBehaviour
         MoveSpeed = 15;
     }
     public int playerNumber = 1;
+    public Rigidbody2D rb;
 
     void Update()
+   
     {
         
         if (playerNumber == 1)
         {
             transform.Translate(new Vector3(0, Input.GetAxis("Player1") * MoveSpeed * Time.deltaTime, 0));
+            if (rb.position.y < -3.6f)
+            {
+                transform.Translate(Vector2.up * 0.1f);
+            }
+            if (rb.position.y > 3.6f)
+            {
+                transform.Translate(Vector2.down * 0.1f);
+            }
         }
         if (playerNumber == 2)
         {
             transform.Translate(new Vector3(0, Input.GetAxis("Player2") * MoveSpeed * Time.deltaTime, 0));
+            if (rb.position.y < -3.6f)
+            {
+                transform.Translate(Vector2.up * 0.1f);
+            }
+            if (rb.position.y > 3.6f)
+            {
+                transform.Translate(Vector2.down * 0.1f);
+            }
         }
+
     }
 }
